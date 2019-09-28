7fd24d0d84a578ee5899317872d941b4dcfdf7e4
dc_sherlock <weihongxv@163.com>
2019-09-28 19:23:37 +0800

    05

--- go/solution/5.最长回文子串.go
+++ go/solution/5.最长回文子串.go
@@ -4,6 +4,36 @@
  * [5] 最长回文子串
  */
 func longestPalindrome(s string) string {
-    
+	res := ""
+	var start, end, length int = 0, 0, 0
+	var substr string
+	if len(s) == 0 {
+		return res
+	}
+	for i := 0; i < len(s); i++ {
+		len1 := rever(i, i, s)
+		len2 := rever(i, i+1, s)
+		if len1 > len2 {
+			length = len1
+		} else {
+			length = len2
+		}
+		if length > end-start {
+			end = i + length/2
+			start = i - (length-1)/2
+			substr = s[start : end+1] //因为是向下取整
+		}
+	}
+	return substr
 }
-
+func rever(left int, right int, s string) int {
+	for left >= 0 && right < len(s) {
+		if s[left] == s[right] {
+			left--
+			right++
+		} else {
+			break
+		}
+	}
+	return right - left - 1
+}
